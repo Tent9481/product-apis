dc62465dad8f6e9671c2d8154b85c442babbd45a
yashas <120266894+Tent9481@users.noreply.github.com>
2025-08-21 11:56:53 +0530

    Create step7.js

---
+++ step7.js
@@ -0,0 +1,18 @@
+{
+  "product_name": "Sample Product",
+  "brand": {
+    "name": "Sample Brand",
+    "year_founded": "2020",
+    "company_age": "4",
+    "address": "123 Sample Street, Sample City, Sample State, 12345, Sample Country"
+  },
+  "category_name": "Electronics",
+  "description_text": "Sample description",
+  "price": "999.99",
+  "currency": "USD",
+  "processor": "Sample Processor",
+  "memory": "16GB",
+  "release_date": "2024-01-15",
+  "average_rating": "4.5",
+  "rating_count": "100"
+}
